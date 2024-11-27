0bd244f86bba51908045ede8c7e96aebb3200deb
level237 <Bramslevel129@gmail.com>
2024-11-27 17:50:04 +0100

    update

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\LogoutController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -15,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 Route::post("login",[LoginController::class,"login"]);
+
+Route::middleware(['auth:api'])->prefix('v1')->group(function(){
+    Route::post('/logout',[LogoutController::class,'logout']);
+});
