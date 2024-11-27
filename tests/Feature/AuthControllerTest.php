45d3232bc40d31e64fdd40676bf93740cdf140a8
level237 <Bramslevel129@gmail.com>
2024-11-27 20:49:43 +0100

    update

--- tests/Feature/AuthControllerTest.php
+++ tests/Feature/AuthControllerTest.php
@@ -11,15 +11,25 @@ class AuthControllerTest extends TestCase
     /**
      * A basic feature test example.
      */
-    public function it_can_login_a_user(): void
+    public function test_it_can_login_a_user(): void
     {
         $user = User::find(1);
 
         $response = $this->postJson('/api/login', [
             'phone_number' => $user->phone_number,
-            'password' => $user->password,
+            'password' => "password",
         ]);
 
         $response->assertStatus(200);
+    }
+
+    public function test_it_fails_login_with_invalid_credentials()
+    {
+        $response = $this->postJson('/api/login', [
+            'phone_number' => "6634542432",
+            'password' => "wrongpassword",
+        ]);
+
+        $response->assertStatus(400);
     }
 }
