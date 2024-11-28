acdec89293a059e458c229b4edbad3fc8226daa5
level237 <Bramslevel129@gmail.com>
2024-11-28 22:37:53 +0100

    update model and migration

---
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\CategoryRequest;
+use App\Models\Category;
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(CategoryRequest $request)
+    {
+        try{
+            $category=new Category;
+            $category->category_name=$request->category_name;
+            if($request->parent_id){
+                $category->parent_id=$request->parent_id;
+            }
+            $category->save();
+            return response()->json(['message'=>"Category created successfully"],200);
+        }catch(\Exception $e){
+            return response()->json([
+                'success' => false,
+                'message' => 'Something went wrong',
+                'errors' => $e
+              ], 500);
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
--- database/migrations/2024_11_26_193312_create_categories_table.php
+++ database/migrations/2024_11_26_193312_create_categories_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->string('category_name');
+            $table->foreignId('parent_id')->nullable()->constrained()->constrained('categories')->onDelete('cascade');
             $table->timestamps();
         });
     }
