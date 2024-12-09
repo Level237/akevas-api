a76bfe4cfd8dadc6faa258b4ee0e4d812a0395c4
level237 <Bramslevel129@gmail.com>
2024-12-09 11:29:12 +0100

    Subscription Product in action

--- app/Http/Controllers/Payment/Coolpay/Product/SubscribeProductController.php
+++ app/Http/Controllers/Payment/Coolpay/Product/SubscribeProductController.php
@@ -3,10 +3,12 @@
 namespace App\Http\Controllers\Payment\Coolpay\Product;
 
 use App\Http\Controllers\Controller;
+use App\Models\Payment;
 use App\Models\Product;
 use App\Models\Subscription;
 use App\Services\Payment\Coolpay\InitPaymentService;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class SubscribeProductController extends Controller
 {
@@ -17,8 +19,25 @@ class SubscribeProductController extends Controller
         return $initResponse;
     }
 
-    public function paymentPending($membership_id,$product_id){
+    public function paymentPending($membership_id,$product_id,$transaction_ref){
         $subscription_type=Subscription::find($membership_id);
         $product=Product::find($product_id);
+        $paymentExist=Payment::where('transaction_ref',$transaction_ref)->first();
+
+        if(!isset($paymentExist)){
+            $data=[
+                'payment_type'=>"Momo",
+                'price'=>$subscription_type->subscription_price,
+                'payment_of'=>"Abonnement Produit",
+                'transaction_ref'=>$transaction_ref,
+                'transaction_id'=>null,
+                'membership_id'=>$membership_id,
+                'product_id'=>$product_id,
+                'status'=>"0",
+                'user_id'=>Auth::user()->id
+            ];
+            //$payment=event(new MakePayment($data));
+            return response()->json(['message'=>"payment Pending"]);
+    }
     }
 }
