a956090cf721b0346573248ba6d434a0014175d5
level237 <Bramslevel129@gmail.com>
2025-02-10 19:03:08 +0100

    auth in action

--- app/Http/Controllers/Auth/LogoutController.php
+++ app/Http/Controllers/Auth/LogoutController.php
@@ -4,13 +4,12 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
+
 
 class LogoutController extends Controller
 {
     public function logout(){
-        $user = Auth::guard('api')->user();
-        $user->token()->revoke();
+      
         return response()->json(['message'=>"Logout successfully"]);
     }
 }
