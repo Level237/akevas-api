9b47821104918b988953c236204dc7959bdae6dc
level237 <Bramslevel129@gmail.com>
2025-02-12 08:35:03 +0100

    update

--- database/seeders/QuarterSeeder.php
+++ database/seeders/QuarterSeeder.php
@@ -12,6 +12,206 @@ class QuarterSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Quarter::create([
+            'quarter_name'=>"Akwa",
+            'town_id'=>1
+        ]);
+
+        Quarter::create([
+            'quarter_name'=>"Bali",
+            'town_id'=>1
+        ]);
+        Quarter::create([
+            'quarter_name'=>"Bépanda",
+            'town_id'=>1
+        ]);
+        Quarter::create([
+            'quarter_name'=>"Deïdo",
+            'town_id'=>1
+        ]);
+        Quarter::create([
+            'quarter_name'=>"Bonapriso",
+            'town_id'=>1
+        ]);
+        Quarter::create([
+            'quarter_name'=>"Bonaberi",
+            'town_id'=>1
+        ]);
+        Quarter::create([
+            'quarter_name'=>"Bonandjo",
+            'town_id'=>1
+        ]);
+        Quarter::create([
+            'quarter_name'=>"Bonamoussadi",
+            'town_id'=>1
+        ]);
+        Quarter::create([
+            'quarter_name'=>"Denver, Santa Barbara",
+            'town_id'=>1
+        ]);
+        Quarter::create([
+            'quarter_name'=>"Kotto",
+            'town_id'=>1
+        ]);
+        Quarter::create([
+            'quarter_name'=>"Kotto",
+            'town_id'=>1
+        ]);
+        Quarter::create([
+            'quarter_name'=>"New bell",
+            'town_id'=>1
+        ]);
+        Quarter::create([
+            'quarter_name'=>"Zone Bassa",
+            'town_id'=>1
+        ]);
+        Quarter::create([
+            'quarter_name'=>"Cite des Palmiers",
+            'town_id'=>1
+        ]);
+        Quarter::create([
+            'quarter_name'=>"Camp Yabassi",
+            'town_id'=>1
+        ]);
+        Quarter::create([
+            'quarter_name'=>"Cité Sic",
+            'town_id'=>1
+        ]);
+        Quarter::create([
+            'quarter_name'=>"Dakar",
+            'town_id'=>1
+        ]);
+        Quarter::create([
+            'quarter_name'=>"Essec",
+            'town_id'=>1
+        ]);
+        Quarter::create([
+            'quarter_name'=>"Japoma",
+            'town_id'=>1
+        ]);
+        Quarter::create([
+            'quarter_name'=>"Kotto",
+            'town_id'=>1
+        ]);
+        Quarter::create([
+            'quarter_name'=>"Logbessou",
+            'town_id'=>1
+        ]);
+        Quarter::create([
+            'quarter_name'=>"Logbaba",
+            'town_id'=>1
+        ]);
+        Quarter::create([
+            'quarter_name'=>"Maképé",
+            'town_id'=>1
+        ]);
+        Quarter::create([
+            'quarter_name'=>"Mbangué",
+            'town_id'=>1
+        ]);
+        Quarter::create([
+            'quarter_name'=>"Mimongo",
+            'town_id'=>1
+        ]);
+        Quarter::create([
+            'quarter_name'=>"Ndogbong",
+            'town_id'=>1
+        ]);
+        Quarter::create([
+            'quarter_name'=>"Ndockoti",
+            'town_id'=>1
+        ]);
+        Quarter::create([
+            'quarter_name'=>"Nyalla",
+            'town_id'=>1
+        ]);
+        Quarter::create([
+            'quarter_name'=>"Village",
+            'town_id'=>1
+        ]);
+        Quarter::create([
+            'quarter_name'=>"Yassa",
+            'town_id'=>1
+        ]);
+        Quarter::create([
+            'quarter_name'=>"Akonolinga",
+            'town_id'=>2
+        ]);
+        Quarter::create([
+            'quarter_name'=>"Bastos",
+            'town_id'=>2
+        ]);
+        Quarter::create([
+            'quarter_name'=>"Biyem-Assi",
+            'town_id'=>2
+        ]);
+        Quarter::create([
+            'quarter_name'=>"Biyem-Assi",
+            'town_id'=>2
+        ]);
+        Quarter::create([
+            'quarter_name'=>"Briqueterie",
+            'town_id'=>2
+        ]);
+        Quarter::create([
+            'quarter_name'=>"Elig Effa",
+            'town_id'=>2
+        ]);
+        Quarter::create([
+            'quarter_name'=>"Elig Edzoa",
+            'town_id'=>2
+        ]);
+        Quarter::create([
+            'quarter_name'=>"Elig Essona",
+            'town_id'=>2
+        ]);
+        Quarter::create([
+            'quarter_name'=>"Kodengui",
+            'town_id'=>2
+        ]);
+        Quarter::create([
+            'quarter_name'=>"Melen",
+            'town_id'=>2
+        ]);
+        Quarter::create([
+            'quarter_name'=>"Messassi",
+            'town_id'=>2
+        ]);
+        Quarter::create([
+            'quarter_name'=>"Mvog Ada",
+            'town_id'=>2
+        ]);
+        Quarter::create([
+            'quarter_name'=>"Mvog Atangana Mballa",
+            'town_id'=>2
+        ]);
+        Quarter::create([
+            'quarter_name'=>"Mvog Mbi",
+            'town_id'=>2
+        ]);
+        Quarter::create([
+            'quarter_name'=>"Nkoldongo",
+            'town_id'=>2
+        ]);
+        Quarter::create([
+            'quarter_name'=>"Ngoa Ekele",
+            'town_id'=>2
+        ]);
+        Quarter::create([
+            'quarter_name'=>"Ntoungou",
+            'town_id'=>2
+        ]);
+        Quarter::create([
+            'quarter_name'=>"Miboman",
+            'town_id'=>2
+        ]);
+        Quarter::create([
+            'quarter_name'=>"Etoudi",
+            'town_id'=>2
+        ]);
+        Quarter::create([
+            'quarter_name'=>"Elozoa",
+            'town_id'=>2
+        ]);
     }
 }
