20a8b29322bf8036f53552ee82ad3f9b5d480de2
level237 <Bramslevel129@gmail.com>
2025-02-20 17:31:01 +0100

    list product by gender

--- app/Http/Resources/GenderResource.php
+++ app/Http/Resources/GenderResource.php
@@ -3,10 +3,12 @@
 namespace App\Http\Resources;
 
 use App\Models\Shop;
+use App\Models\Product;
 use App\Models\Category;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\URL;
 use App\Http\Resources\ShopResource;
+use App\Http\Resources\ProductResource;
 use App\Http\Resources\CategoryResource;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -27,6 +29,7 @@ class GenderResource extends JsonResource
             'categories' => CategoryResource::collection(Category::whereHas('genders', function($query) {
                 $query->where('gender_id', $this->id);
             })->where('parent_id', null)->get()),
+            'products' => ProductResource::collection(Product::where('product_gender',$this->id)->orWhere('product_gender',4)->take(4)->get()),
             'shops' => ShopResource::collection(Shop::where('shop_gender',$this->id)->orWhere('shop_gender',4)->take(7)->get()),
         ];
     }
