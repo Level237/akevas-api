ea6f4a7dac26fd0928e0cc574dfb20badd328f73
level237 <Bramslevel129@gmail.com>
2025-03-03 18:26:26 +0100

    update

--- app/Http/Controllers/Payment/Stripe/PaymentController.php
+++ app/Http/Controllers/Payment/Stripe/PaymentController.php
@@ -24,26 +24,31 @@ class PaymentController extends Controller
                 'source' => $request->stripeToken,
                 'description' => 'Achat d\'un produit',
             ]);
-            foreach($request->productsPayments as $product){
-                $order=new Order;
-            $order->user_id=Auth::guard("api")->user()->id;
-            $order->isPay=1;
-            $order->total=$request->amount;
-            $order->fee_of_shipping=$request->shipping;
-            $order->payment_method="0";
-            if($order->save()){
-                $orderDetails=new OrderDetail;
-                $orderDetails->order_id=$order->id;
-                $orderDetails->product_id=$product['product_id'];
-                $orderDetails->order_product_quantity=$product['quantity'];
-                $orderDetails->unit_price=$product['price'];
-                $orderDetails->save();
-            }
+
+            if(isset($request->productsPayments)){
+                foreach($request->productsPayments as $product){
+                    $order=$this->createOrder($request->amount,
+                    $request->shipping,
+                    $product['product_id'],
+                    $product['quantity'],
+                    $product['price']);
+                }
              return response()->json([
                 'success' => true,
                 'message' => 'Payment successful',
                 'order' => $order,
             ], 200);
+            }else{
+                $order=$this->createOrder($request->amount,
+                $request->shipping,
+                $request->productId,
+                $request->quantity,
+                $request->amount);
+                return response()->json([
+                    'success' => true,
+                    'message' => 'Payment successful',
+                    'order' => $order,
+                ], 200);
             }
             
            
@@ -58,5 +63,23 @@ class PaymentController extends Controller
 
     function calculateRealNumber($amount) {
         return (($amount)*100);
+    }
+    private function createOrder($amount,$shipping,$productId,$quantity,$price){
+
+         $order=new Order;
+            $order->user_id=Auth::guard("api")->user()->id;
+            $order->isPay=1;
+            $order->total=$amount;
+            $order->fee_of_shipping=$shipping;
+            $order->payment_method="0";
+            if($order->save()){
+                $orderDetails=new OrderDetail;
+                $orderDetails->order_id=$order->id;
+                $orderDetails->product_id=$productId;
+                $orderDetails->order_product_quantity=$quantity;
+                $orderDetails->unit_price=$price;
+                $orderDetails->save();
+            }
+            return $order;
     }
 }
