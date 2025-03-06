5eb3cfc1d678f3716d9488c5716bc6aa346c0456
level237 <Bramslevel129@gmail.com>
2025-03-07 00:06:51 +0100

    Implement order taking process for delivery users

--- app/Http/Controllers/Delivery/TakeOrderProcessController.php
+++ app/Http/Controllers/Delivery/TakeOrderProcessController.php
@@ -2,18 +2,23 @@
 
 namespace App\Http\Controllers\Delivery;
 
-use App\Http\Controllers\Controller;
-use App\Models\Delivery;
+use App\Models\User;
+
+use App\Models\Order;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
 
 class TakeOrderProcessController extends Controller
 {
     public function take($order_id){
 
-        $newDelivery=new Delivery;
-        $newDelivery->order_id=$order_id;
-        $newDelivery->user_id=Auth::guard('api')->user()->id;
+        $delivery=User::find(Auth::guard('api')->user()->id);
+        $order=Order::find($order_id);
+        $order->status="1";
+        $order->isTake=1;
+        $order->save();
+        $delivery->processOrders()->attach($order_id,['isAccepted'=>true]);
         return response()->json(["message"=>"Order accept"]);
     }
 }
