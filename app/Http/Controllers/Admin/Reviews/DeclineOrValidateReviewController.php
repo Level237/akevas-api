a5d3b264099ed18e1bc5e395af12385c593329eb
level237 <Bramslevel129@gmail.com>
2025-03-14 17:09:18 +0100

    Refactor DeclineOrValidateReviewController to retrieve review by ID before updating approval status

--- app/Http/Controllers/Admin/Reviews/DeclineOrValidateReviewController.php
+++ app/Http/Controllers/Admin/Reviews/DeclineOrValidateReviewController.php
@@ -9,7 +9,7 @@ use App\Http\Controllers\Controller;
 class DeclineOrValidateReviewController extends Controller
 {
     public function declineOrValidate($review_id,$status){
-        $review=new Review;
+        $review=Review::find($review_id);
         $review->is_approved=$status;
         $review->save();
         return response()->json(['message'=>true]);
