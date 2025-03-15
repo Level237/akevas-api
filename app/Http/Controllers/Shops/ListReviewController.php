39d11ae092c73fca7e63ee00da07c7f65aff05fa
level237 <Bramslevel129@gmail.com>
2025-03-15 11:25:58 +0100

    Filter shop reviews to only include approved ones in ListReviewController

--- app/Http/Controllers/Shops/ListReviewController.php
+++ app/Http/Controllers/Shops/ListReviewController.php
@@ -10,7 +10,7 @@ use App\Http\Resources\ShopReviewResource;
 class ListReviewController extends Controller
 {
     public function index($shop_id){
-        $reviews=ShopReview::where('shop_id',$shop_id)->get();
+        $reviews=ShopReview::where('shop_id',$shop_id)->where('is_approved',1)->get();
 
         return response()->json(ShopReviewResource::collection($reviews));
     }
