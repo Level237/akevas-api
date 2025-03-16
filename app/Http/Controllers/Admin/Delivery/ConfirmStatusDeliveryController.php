b10993d0a4f4242905f6866c7005ec279ab9dbc3
level237 <Bramslevel129@gmail.com>
2025-03-16 14:17:57 +0100

    Enhance ConfirmStatusDeliveryController to handle feedback submission and error management during delivery status confirmation

--- app/Http/Controllers/Admin/Delivery/ConfirmStatusDeliveryController.php
+++ app/Http/Controllers/Admin/Delivery/ConfirmStatusDeliveryController.php
@@ -4,15 +4,33 @@ namespace App\Http\Controllers\Admin\Delivery;
 
 use App\Models\User;
 use App\Models\Delivery;
+use App\Models\FeedBack;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
 class ConfirmStatusDeliveryController extends Controller
 {
     public function confirmStatusDelivery($id,Request $request) {
-        $delivery = User::find($id);
-        $delivery->isDelivery = $request->isDelivery;
-        $delivery->save();
-        return response()->json(['message' => 'Status de livraison confirmé avec succès']);
+
+        try{
+            $delivery = User::find($id);
+
+            if($request->isDelivery==0){
+                $feedBack=new FeedBack;
+                $feedBack->user_id=$delivery->id;
+                $feedBack->message=$request->message;
+                $feedBack->status=0;
+                $feedBack->save();
+            }
+            $delivery->isDelivery = $request->isDelivery;
+            $delivery->save();
+            return response()->json(['message' => 'Status de livraison confirmé avec succès']);
+        }catch (\Exception $e) {
+            return response()->json([
+                'message' => 'Une erreur est survenue',
+                'error' => $e->getMessage()
+            ], 500);
+        }
+        
     }
 }
