d62ab55b0dfc123d98c3c5bcdd72d3e035e7efa0
level237 <Bramslevel129@gmail.com>
2025-03-19 21:37:22 +0100

    Enhance InitPaymentController to include error handling and dynamic parameter extraction for payment initialization; restructure callback URL and add metadata for improved request clarity.

--- app/Http/Controllers/Product/Payment/InitPaymentController.php
+++ app/Http/Controllers/Product/Payment/InitPaymentController.php
@@ -9,20 +9,43 @@ use Illuminate\Support\Facades\Http;
 class InitPaymentController extends Controller
 {
     public function initPayment(Request $request){
-        $url = "https://api.notchpay.co/payments/initialize";
+        try{
+            $url = "https://api.notchpay.co/payments/initialize";
+            $total = $request->total;
+            $shipping = $request->shipping;
+            $productId = $request->productId;
+            $quantity = $request->quantity;
+            $name = $request->name;
+            $price = $request->price;
+            $quarter = $request->quarter;
+            $paymentMethod = $request->paymentMethod;
         $response=Http::acceptJson()->withBody(json_encode(
             [
                 "email"=>"brams@gmail.com",
                 "amount"=>"100",
                 "currency"=>"XAF",
                 "reference"=>"REFID".rand(123456, 999999),
-                "callback"=>"http://localhost:5173?method=".$request->paymentMethod."&total=".$request->total."&shipping=".$request->shipping."&productId=".$request->productId."&quantity=".$request->quantity."&name=".$request->name."&price=".$request->price."&quarter=".$request->quarter,
-                    
+                "callback"=>"http://localhost:5173?method=$paymentMethod&total=$total&shipping=$shipping&productId=$productId&quantity=$quantity&price=$price&quarter=$quarter",
+                "metadata"=>[
+                    "total"=>$total,
+                    "shipping"=>$shipping,
+                    "productId"=>$productId,
+                    "quantity"=>$quantity,
+                    "name"=>$name,
+                    "price"=>$price,
+                    "quarter"=>$quarter,
+                ]
             ]
             ),'application/json')->withHeaders([
                 "Authorization"=>"pk_test.aucN1k448sPoYBdotSJJ6U5IDXDFDFAhUbNSdVguuwHAuhBIGVC0NHTQgEl30m3Xtq83aqpSvq9rFA1VPi7cRUiaKr0fAh64xFLxIqiof4y7tZ6TuJ9FM4cUHs5Av"
             ])->post($url);
 
             return json_decode($response);
+        }catch(Exception $e){
+            return response()->json([
+                "status"=>"error",
+                "message"=>$e->getMessage()
+            ],500);
+        }
     }
 }
