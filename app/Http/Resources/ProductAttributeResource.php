bc58675414e0d0ada3f8b2aa86c1dbc7e01e460b
level237 <Bramslevel129@gmail.com>
2025-03-22 14:48:42 +0100

    Update ProductAttributeResource to retrieve images for product attributes using a database query, enhancing the structure of the returned data.

--- app/Http/Resources/ProductAttributeResource.php
+++ app/Http/Resources/ProductAttributeResource.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Resources;
 
+use App\Models\Image;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\URL;
 use App\Http\Resources\ImageResource;
 use Illuminate\Http\Resources\Json\JsonResource;
@@ -19,7 +21,11 @@ class ProductAttributeResource extends JsonResource
         return [
             'id' => $this->id,
             'variant_name' => $this->pivot->variant_name,
-            'images' => $this->productAttributesValues,
+            'images' => $this->productAttributesValues->map(function ($attribute) {
+                return DB::table('product_attributes_value_image')->where('attributes_id', $attribute->id)->get()->map(function ($image) {
+                    return ImageResource::make(Image::find($image->image_id));
+                });
+            }),
             'quantity' => $this->pivot->quantity,
             'price' => $this->pivot->price,
         ];
