ebe8bd037f37c65e8a6ad2cd91e6251ae19a8600
level237 <Bramslevel129@gmail.com>
2025-03-22 20:31:08 +0100

    Refactor quarter parameter handling in InitPaymentController to remove URL encoding, simplifying data processing for payment initialization.

--- app/Http/Controllers/Product/Payment/InitPaymentController.php
+++ app/Http/Controllers/Product/Payment/InitPaymentController.php
@@ -17,7 +17,7 @@ class InitPaymentController extends Controller
             $quantity = $request->quantity;
             $name = urlencode($request->name);
             $price = $request->price;
-            $quarter = urlencode($request->quarter);
+            $quarter = $request->quarter;
             $paymentMethod = $request->paymentMethod;
         $response=Http::acceptJson()->withBody(json_encode(
             [
