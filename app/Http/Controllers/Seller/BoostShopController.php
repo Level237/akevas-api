a29a1e713af7d52f822847e06a7787f356981632
level237 <Bramslevel129@gmail.com>
2025-04-17 10:26:00 +0200

    Implement boost functionality in BoostShopController, adding subscription handling, payment creation, and error management. Enhance shop level and coin allocation based on subscription type, with improved response structure for success and failure scenarios.

--- app/Http/Controllers/Seller/BoostShopController.php
+++ app/Http/Controllers/Seller/BoostShopController.php
@@ -2,16 +2,21 @@
 
 namespace App\Http\Controllers\Seller;
 
+use Carbon\Carbon;
 use App\Models\Shop;
+use App\Models\Payment;
 use App\Models\Subscription;
 use Illuminate\Http\Request;
+use App\Models\SubscriptionUser;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
 
 class BoostShopController extends Controller
 {
     public function boost(Request $request){
-        $shop = Shop::where('user_id', Auth::guard('api')->user()->id)->first();
+
+        try{
+            $shop = Shop::where('user_id', Auth::guard('api')->user()->id)->first();
         $Subscription=Subscription::find($request->subscription_id);
         if(!shop){
             return response()->json([
@@ -23,9 +28,36 @@ class BoostShopController extends Controller
         $newDateTime->setTimezone('Africa/Douala');
 
         $shop->subscription_id = $Subscription->id;
+        if($shop->shop_level==3){
+            $shop->shop_level=4;
+        }
+        if($Subscription->id==3){
+            $shop->coins=$shop->coins+500;
+        }
         $hop->expire=null;
         $shop->isSubscribe=1;
         $shop->save();
+
+        $payment=new Payment;
+        $payment->payment_type="coins";
+        $payment->price=$coins;
+        $payment->payment_of="Boostage boutique";
+        
+        if($payment->save()){
+        $subscription=new SubscriptionUser;
+        $subscription->user_id=$shop->user_id;
+        $subscription->subscription_id=$Subscription->id;
+        $subscription->expire_at=$newDateTime;
+        $subscription->status=1;
+        $subscription->save();
+
+        return response()->json(["status"=>1]);
+        }else{
+            return response()->json(["status"=>0]);
+        }
+        }catch(\Exception $e){
+            return response()->json(["status"=>0]);
+        }
         
     }
 }
