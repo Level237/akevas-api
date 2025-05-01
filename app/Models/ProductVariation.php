a0aaea8a3faeccf1c45e98b66bd471e440582f7b
level237 <Bramslevel129@gmail.com>
2025-05-01 23:56:33 +0200

    Refactor payment processing logic in InitPaymentController to streamline amount adjustments and ensure consistent handling of phone numbers, improving overall accuracy in payment transactions.

---
+++ app/Models/ProductVariation.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Product;
+use App\Models\AttributeValue;
+use App\Models\VariationAttribute;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class ProductVariation extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['product_id', 'color_id', 'price'];
+
+    protected $casts = [
+        'price' => 'decimal:2'
+    ];
+
+    public function product(): BelongsTo
+    {
+        return $this->belongsTo(Product::class);
+    }
+
+    public function color(): BelongsTo
+    {
+        return $this->belongsTo(AttributeValue::class, 'color_id');
+    }
+
+    public function attributes(): HasMany
+    {
+        return $this->hasMany(VariationAttribute::class);
+    }
+
+    
+}
---
+++ app/Models/VariationAttribute.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\AttributeValue;
+use App\Models\ProductVariation;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class VariationAttribute extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'product_variation_id',
+        'attribute_value_id',
+        'quantity',
+        'price'
+    ];
+
+    protected $casts = [
+        'quantity' => 'integer',
+        'price' => 'decimal:2'
+    ];
+
+    public function variation(): BelongsTo
+    {
+        return $this->belongsTo(ProductVariation::class, 'product_variation_id');
+    }
+
+    public function attributeValue(): BelongsTo
+    {
+        return $this->belongsTo(AttributeValue::class);
+    }
+}
