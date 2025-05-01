637ec52525502a652de72298907a5826238b059d
level237 <Bramslevel129@gmail.com>
2025-05-02 00:32:02 +0200

    Update product variations migration to use foreignIdFor method for product_id, enhancing code clarity and consistency in database schema.

--- database/migrations/2025_05_01_213002_create_product_variations_table.php
+++ database/migrations/2025_05_01_213002_create_product_variations_table.php
@@ -1,8 +1,9 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
+use App\Models\Product;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 return new class extends Migration
 {
@@ -13,7 +14,7 @@ return new class extends Migration
     {
         Schema::create('product_variations', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('product_id')->constrained()->onDelete('cascade');
+            $table->foreignIdFor(Product::class)->constrained()->onDelete('cascade');
             $table->foreignId('color_id')->constrained('attribute_values');
             $table->decimal('price', 10, 2);
             $table->timestamps();
