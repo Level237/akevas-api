92674d467c765253e36cab2e18d483b8ecae9622
level237 <Bramslevel129@gmail.com>
2025-06-07 11:49:31 +0200

    Refactor payment status handling in HandleWebhookController and HandleVerifyPaymentNotchpay to consistently reflect 'complete' status. This update enhances the accuracy of payment processing and aligns with recent changes in payment logic.

---
+++ app/Services/Auth/CreateAccountSyncService.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Services\Auth;
+
+use Illuminate\Support\Facades\Http;
+
+class CreateAccountSyncService {
+
+    public function createSyncAccount($sellerName,$url,$category,$email,$phone,$sellerId){
+
+        $response=Http::acceptJson()->withHeaders([ "Authorization"=>env("NOTCHPAY_API_KEY"),
+        'Content-Type' => 'application/json',
+        'Accept' => 'application/json',
+    ])->post('https://api.notchpay.co/accounts', [
+        'type' => 'standard', // ou 'express', 'custom'
+        'business_profile' => [
+            'name' => $sellerName,
+            'url' => $url,
+            'category' => $category,
+        ],
+        'email' => $email,
+        'phone' => $phone,
+        'metadata' => [
+            'seller_id' => $sellerId,
+            'internal_reference' => "Seller_".$sellerId,
+        ],
+    ]);
+
+    $responseData=json_decode($response);
+
+    return $responseData->account->id;
+
+    }
+}
