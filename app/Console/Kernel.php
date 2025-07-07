3ab0a5a008db77b3cc267354d9bd1e810cead311
level237 <bramslevel129@gmail.com>
2025-07-07 12:51:28 +0100

    Update console scheduling to replace hourly job with minute-based queue processing
    Comment out the hourly job for checking subscription products and add a new command to process the queue every minute, improving task execution frequency.

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,8 +13,9 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        $schedule->job(new JobCheckSubscriptionProduct)->hourly();
+        //$schedule->job(new JobCheckSubscriptionProduct)->hourly();
         // $schedule->command('inspire')->hourly();
+        $schedule->command('queue:work --stop-when-empty')->everyMinute();
     }
 
     /**
