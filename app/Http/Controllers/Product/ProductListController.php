e7da13f12718d8f08c4792cc32dd65faefeb938d
level237 <bramslevel129@gmail.com>
2025-09-12 19:31:24 +0100

    Enhance allProducts method in ProductListController to support price filtering
    This commit updates the allProducts method to allow filtering of products based on minimum and maximum price parameters. It introduces a query builder that checks for price constraints on both simple products and their variations, improving the product listing functionality and user experience.

--- app/Http/Controllers/Product/ProductListController.php
+++ app/Http/Controllers/Product/ProductListController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Resources\ProductResource;
 use App\Models\Product;
 use Illuminate\Http\Request;
+use Illuminate\Database\Eloquent\Builder;
 
 class ProductListController extends Controller
 {
@@ -16,7 +17,42 @@ class ProductListController extends Controller
     public function adsProducts($id){
         return ProductResource::collection(Product::inRandomOrder()->Where('subscribe_id',$id)->where('status',1)->where('is_trashed',0)->get());
     }
-    public function allProducts(){
-        return ProductResource::collection(Product::inRandomOrder()->where('status',1)->where('is_trashed',0)->paginate(6));
+    public function allProducts(Request $request){
+        $query = Product::inRandomOrder()
+        ->where('status', 1)
+        ->where('is_trashed', 0);
+        if ($request->has('min_price') || $request->has('max_price')) {
+            $query->where(function (Builder $subQuery) use ($request) {
+                
+                // Logique pour les produits simples
+                $subQuery->where(function ($simpleProductQuery) use ($request) {
+                    if ($request->has('min_price')) {
+                        $minPrice = floatval($request->input('min_price'));
+                        $simpleProductQuery->whereRaw('CAST(product_price AS DECIMAL(10, 2)) >= ?', [$minPrice]);
+                    }
+                    if ($request->has('max_price')) {
+                        $maxPrice = floatval($request->input('max_price'));
+                        $simpleProductQuery->whereRaw('CAST(product_price AS DECIMAL(10, 2)) <= ?', [$maxPrice]);
+                    }
+                });
+    
+                // Logique pour les produits variés
+                $subQuery->orWhereHas('variations', function (Builder $variationQuery) use ($request) {
+                    if ($request->has('min_price')) {
+                        $minPrice = floatval($request->input('min_price'));
+                        $variationQuery->whereRaw('CAST(price AS DECIMAL(10, 2)) >= ?', [$minPrice]);
+                    }
+                    if ($request->has('max_price')) {
+                        $maxPrice = floatval($request->input('max_price'));
+                        $variationQuery->whereRaw('CAST(price AS DECIMAL(10, 2)) <= ?', [$maxPrice]);
+                    }
+                });
+            });
+        }
+
+
+        $products = $query->paginate(6);
+
+        return ProductResource::collection($products);
     }
 }
